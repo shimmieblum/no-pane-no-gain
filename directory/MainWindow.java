28076ad7f99cd39cacff611cbbc4fa0c0a23ed50
shimmieblum <48183324+shimmieblum@users.noreply.github.com>
2019-03-17 17:53:31 +0000

    Created directory to be used in intelliJ Created main method to show one line of array list of properties.
    created class: MainWindow to store the window and all the different scenes. Each scene is a different panel. they are each created with their own class.
    
    see setter methods to set each scene as the Scene of the window.

---
+++ directory/MainWindow.java
@@ -0,0 +1,68 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * this class simply extends the JavaFX Application class and allows for the creation of four
+ * separate scenes to be placed in the stage. These scenes are created in their own classes.
+ *
+ */
+public class MainWindow extends Application {
+    // the stage
+    private Stage window;
+    // 4 Scenes to show 4 different displays.
+    private Scene welcomeScene, mapScene, statsScene, challengeScene;
+
+    public MainWindow(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * start the Application off, this method is called on construction by the
+     * launch method in the constructor
+     * @param stage the stage.
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        window = stage;
+        // create each Scene with its own class.
+        welcomeScene = new WelcomeScene();
+        mapScene = new MapScene();
+        statsScene = new StatsScene();
+        challengeScene = new ChallengeScene();
+        // the user opens onto the welcome scene.
+        setWelcomeScene();
+        // show the scene
+        window.show();
+    }
+
+    /**
+     * set scene to the welcome scene.
+     */
+    public void setWelcomeScene() {
+        window.setScene(welcomeScene);
+    }
+
+    /**
+     * set scene to the map scene
+     */
+    public void setMapScene(){
+        window.setScene(mapScene);
+    }
+
+    /**
+     * set the scene to the stats scene
+     */
+    public void setStatsScene(){
+        window.setScene(statsScene);
+    }
+
+    /**
+     * set the scene to the challenge scene.
+     */
+    public void setChallengeScene() {
+        window.setScene(challengeScene);
+    }
+
+}
