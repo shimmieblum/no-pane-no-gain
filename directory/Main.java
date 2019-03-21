3aa26bb0cfae58328ddc5b906d1a5338d1f85342
j-a-o <44791149+j-a-o@users.noreply.github.com>
2019-03-21 14:29:44 +0000

    Update Main.java

--- directory/Main.java
+++ directory/Main.java
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        AirbnbDataLoader loader = new AirbnbDataLoader();
-        ArrayList<AirbnbListing> list = loader.load();
-        System.out.println(list.get(2));
-    }
-}
-=======
-import java.util.ArrayList;
 
-public class Main {
     public static void main(String[] args) {
-        AirbnbDataLoader loader = new AirbnbDataLoader();
-        ArrayList<AirbnbListing> list = loader.load();
-        System.out.println(list.get(2));
+
+        MainWindow mainWindow = new MainWindow(args);
+
     }
 }
->>>>>>> Shimmie
+
