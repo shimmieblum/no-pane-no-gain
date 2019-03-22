5dbca699d384d8600138a575a1819fcad1bcf14e
j-a-o <44791149+j-a-o@users.noreply.github.com>
2019-03-22 15:19:01 +0000

    Update Statistics.java

--- directory/Statistics.java
+++ directory/Statistics.java
@@ -1,3 +1,5 @@
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,14 +9,20 @@ public class Statistics {
 
     private Listings listings;
     private AirbnbListing currentListing;
+    //all the statistics
     private Map<String, Object> stats;
 
+    // the keys of currently displayed statistics - always 4
+    private List<String> usedStats;
+    //the keys of currently unused statistics - always 4
+    private List<String> unusedStats;
 
-    public Statistics(Listings l, AirbnbListing c) {
+
+    public Statistics(Listings l) {
         listings = l;
-        currentListing = c;
         stats = new HashMap<>();
         createStats();
+        createStatGroups();
     }
 
     private void createStats() {
@@ -33,22 +41,22 @@ public class Statistics {
     public void averageReviewsPerProperty() {
         int totalReviews = 0;
         int avgNumberReviews = 0;
-        for (int i = 0; i < listings.getNumberOfProperties(); i++) {
+        for (int i = 0; i < listings.numberOfProperties(); i++) {
             totalReviews += listings.getProperty(i).getNumberOfReviews();
         }
-        avgNumberReviews = totalReviews/listings.getNumberOfProperties();
+        avgNumberReviews = totalReviews/listings.numberOfProperties();
         stats.put("Average Reviews Per Property", avgNumberReviews);
     }
 
     // adds the number of total available properties to the stats HashMap
     public void totalAvailableProperties() {
-        stats.put("Total Available Properties", listings.getNumberOfProperties());
+        stats.put("Total Available Properties", listings.numberOfProperties());
     }
 
     // adds the number of entire homes and apartments to the stats HashMap
     public void numberHomeApartments() {
         int totalHomeApartments = 0;
-        for (int i = 0; i < listings.getNumberOfProperties(); i++) {
+        for (int i = 0; i < listings.numberOfProperties(); i++) {
             if (listings.getProperty(i).getRoom_type().equals("Entire Home") || listings.getProperty(i).getRoom_type().equals("apt")) {
                 totalHomeApartments++;
             }
@@ -65,7 +73,7 @@ public class Statistics {
         int boroughTotalPrice = 0;
         int largestTotalPrice = 0;
         // find total price of properties in a borough
-        for (int i = 0; i < listings.getNumberOfProperties(); i++) {
+        for (int i = 0; i < listings.numberOfProperties(); i++) {
             if (listings.getProperty(i).getNeighbourhood().equals(currentListing.getNeighbourhood())) {
                 boroughTotalPrice += (currentListing.getMinimumNights()*currentListing.getPrice());
             }
@@ -75,7 +83,8 @@ public class Statistics {
                     largestTotalPrice = n;
                 }
             }
-            stats.put("Total Available Properties", listings.getProperty(i).getNeighbourhood());
+            stats.put("Total Available Properties", listings.getProperty(i).getNeighbourhood()); //TODO can't be inside the loop (but also have u tested its functionality?)
+            //maybe create local data structure to store this instead? TODO same for other loop ones 
         }
     }
 
@@ -84,7 +93,7 @@ public class Statistics {
         int boroughTotalPrice = 0;
         int smallestTotalPrice = 0;
         // find total price of properties in a borough
-        for (int i = 0; i < listings.getNumberOfProperties(); i++) {
+        for (int i = 0; i < listings.numberOfProperties(); i++) {
             if (listings.getProperty(i).getNeighbourhood().equals(currentListing.getNeighbourhood())) {
                 boroughTotalPrice += (currentListing.getMinimumNights()*currentListing.getPrice());
             }
@@ -94,7 +103,7 @@ public class Statistics {
                     smallestTotalPrice = n;
                 }
             }
-            stats.put("Total Available Properties", listings.getProperty(i).getNeighbourhood());
+            stats.put("Total Available Properties", listings.getProperty(i).getNeighbourhood()); //TODO can't be inside the loop (but also have u tested its functionality?)
         }
     }
 
@@ -102,13 +111,13 @@ public class Statistics {
     // calculatedHostListingsCount in AirbnbListing returns the total number of listings the host holds across AirBnB
     public void mostReviewedHost() {
         int greatestNumberOfReviews = 0;
-        for (int i = 0; i < listings.getNumberOfProperties(); i++) {
+        for (int i = 0; i < listings.numberOfProperties(); i++) {
             for (int n = 0; n <= listings.getProperty(i).getNumberOfReviews(); n++){
                 if (n > greatestNumberOfReviews) {
                     greatestNumberOfReviews = n;
                 }
             }
-            stats.put("Most Reviewed Host", listings.getProperty(i).getHost_name());
+            stats.put("Most Reviewed Host", listings.getProperty(i).getHost_name()); //TODO can't be inside the loop (but also have u tested its functionality?)
         }
     }
 
@@ -116,14 +125,14 @@ public class Statistics {
     public void mostExpensiveProperty() {
         int propertyTotalPrice = 0;
         int mostExpensiveProperty = 0;
-        for (int i = 0; i < listings.getNumberOfProperties(); i++) {
+        for (int i = 0; i < listings.numberOfProperties(); i++) {
             propertyTotalPrice = (listings.getProperty(i).getMinimumNights()*listings.getProperty(i).getPrice());
             for (int n = 0; n <= propertyTotalPrice; n++){
                 if (n > mostExpensiveProperty) {
                     mostExpensiveProperty = n;
                 }
             }
-            stats.put("Most Expensive Property", listings.getProperty(i).getName());
+            stats.put("Most Expensive Property", listings.getProperty(i).getName()); //TODO can't be inside the loop (but also have u tested its functionality?)
         }
     }
 
@@ -131,18 +140,46 @@ public class Statistics {
     public void leastExpensiveProperty() {
         int propertyTotalPrice = 0;
         int leastExpensiveProperty = 0;
-        for (int i = 0; i < listings.getNumberOfProperties(); i++) {
+        for (int i = 0; i < listings.numberOfProperties(); i++) {
             propertyTotalPrice = (listings.getProperty(i).getMinimumNights()*listings.getProperty(i).getPrice());
             for (int n = 0; n <= propertyTotalPrice; n++){
                 if (n < leastExpensiveProperty) {
                     leastExpensiveProperty = n;
                 }
             }
-            stats.put("Least Expensive Property", listings.getProperty(i).getName());
+            stats.put("Least Expensive Property", listings.getProperty(i).getName()); //TODO can't be inside the loop (but also have u tested its functionality?)
         }
     }
-
+     /*
+        Returns all the statistics in one HashMap.
+        @return the hashmap containing statistics.
+     */
     public Map<String, Object> getStats() {
         return stats;
+    }
+
+    /*
+        Divides the statistics into two groups, one of which is displayed and one which is waiting to be shown.
+     */
+    private void createStatGroups() {
+        usedStats = new ArrayList<>();
+        unusedStats = new ArrayList(stats.keySet());
+
+    }
+
+    /*
+        Returns the next statistic to be displayed. It can be used to initialise a pane.
+        @param currentStat the current statistic that needs to be changed. Null if there is no previous stat.
+        @return the new Pair of String and Object to be displayed on the screen.
+    */
+    public Pair<String, Object> nextStat(String currentStat) {
+        Pair<String, Object> pair;
+
+        if(currentStat != null) unusedStats.add(currentStat); //adds the current statistic back into the unused list
+
+        String next = unusedStats.get(0);
+        usedStats.add(next); // adds the new statistic into the used list
+        pair = new Pair<>(next, stats.get(next));
+        return pair;
     }
 }
